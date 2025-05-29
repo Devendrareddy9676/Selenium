49b0ff150233f6f7ae6f3bbeceb2eecb7c38084d
Devendrareddy9676 <devendrareddy9676@gmail.com>
2025-05-29 11:25:37 +0530

    first project

---
+++ DataTypeDemo.java
@@ -0,0 +1,25 @@
+package DemoPrpject;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100 ,b=200;
+		System.out.println("the value of a is:"+a);
+		System.out.println("the value of b is:"+b);
+		System.out.println("the value of a and b is:"+a+" "+b);
+		System.out.println("the sum of a and b is:"+(a+b));
+		System.out.println(a+b);
+		
+		byte by=125;
+		System.out.println(by);
+		
+		short sh=2323;
+		System.out.println(sh);
+		
+		long l=282888898547828282l;
+		System.out.println(l);
+
+	}
+
+}
